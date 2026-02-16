f4c3e602fe9b4d3beda019ed3f6f4e67fa12e7e9
NathanaelGracedo <nathanjuan56@gmail.com>
2026-02-16 19:44:15 +0700

    Fix: Convert my-app from submodule to regular folder

---
+++ week_1/my-app/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Praktikum Next.js Pages Router</h1> <br />
+      <p>Nathanael Juan Gracedo-TI3E-16</p>
+    </div>
+  )
+}

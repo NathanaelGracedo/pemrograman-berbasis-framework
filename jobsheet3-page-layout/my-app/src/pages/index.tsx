1f801ab7e227adb7aadd73e28ca55591408bb33f
NathanaelGracedo <nathanjuan56@gmail.com>
2026-02-21 14:54:27 +0700

    Jobsheet 3 - Langkah 5 - Membuat Komponen Navbar

--- jobsheet3-page-layout/my-app/src/pages/index.tsx
+++ jobsheet3-page-layout/my-app/src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Navbar from '@/components/layouts/navbar'
 
 import Link from 'next/link'
 
@@ -9,47 +10,10 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div className={inter.className} style={{
-      minHeight: '100vh',
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center',
-      padding: '20px'
-    }}>
+    <div>
       
-      <div style={{
-        backgroundColor: '#f9f9f9',
-        padding: '40px',
-        borderRadius: '10px',
-        boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
-        maxWidth: '500px',
-        width: '100%',
-        textAlign: 'center'
-      }}>
-        <h1 style={{ 
-          color: '#333', 
-          marginBottom: '20px',
-          fontSize: '2rem'
-        }}>
-          Praktikum Next.js Pages Router
-        </h1>
-        
-        <div style={{ marginTop: '20px' }}>
-          <Link href="/about" style={{
-            backgroundColor: '#0070f3',
-            color: 'white',
-            textDecoration: 'none',
-            fontSize: '18px',
-            padding: '12px 30px',
-            borderRadius: '8px',
-            display: 'inline-block',
-            fontWeight: '400',
-          }}>
-            Lihat Profil Saya 
-          </Link>
-        </div>
-      </div>
+      <h1>Praktikum Next.js Pages Router</h1> <br />
+      <p>Nathanael Juan Gracedo</p>
     </div>
   )
 }

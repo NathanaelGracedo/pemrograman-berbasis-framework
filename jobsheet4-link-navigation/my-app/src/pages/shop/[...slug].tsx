e32fea22782ad41b38ae857a6c0a956d3739105f
NathanaelGracedo <nathanjuan56@gmail.com>
2026-02-23 17:06:28 +0700

    Jobsheet 4 - Langkah 1 & 2 - Membuat Catch-All Route

---
+++ jobsheet4-link-navigation/my-app/src/pages/shop/[...slug].tsx
@@ -0,0 +1,15 @@
+import { useRouter } from "next/router";
+
+const HalamanToko = () => {
+    const Router = useRouter();
+    console.log(Router);
+    const {query} = useRouter();
+    return(
+        <div>
+            <h1>Halaman Toko</h1>
+            <p>Toko: {query.slug && query.slug[0]+"-"+query.slug[1]}</p>
+        </div>
+    );
+};
+
+export default HalamanToko;

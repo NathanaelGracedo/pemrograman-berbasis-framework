0a44f7462488bd45eed58c3796a66febd89484a8
NathanaelGracedo <nathanjuan56@gmail.com>
2026-02-26 16:25:07 +0700

    Jobsheet 5 - Langkah 3 - Styling untuk Pages (CSS Module)

--- jobsheet5-styling-page/my-app/src/pages/auth/login.tsx
+++ jobsheet5-styling-page/my-app/src/pages/auth/login.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import styles from "./login.module.css";
 
 const HalamanLogin = () => {
     const { push } = useRouter();
     
-    const handleLogin = () => {
+    const handlerLogin = () => {
         // Simulasi logic login
         console.log("Processing login...");
         
@@ -13,34 +14,10 @@ const HalamanLogin = () => {
     };
 
     return(
-        <div style={{ padding: "20px", maxWidth: "400px", margin: "0 auto" }}>
+        <div className={styles.login}>
             <h1>Halaman Login</h1>
-            
-            <div style={{ marginBottom: "20px" }}>
-                {/* Navigasi Imperatif: Login → Product */}
-                <button 
-                    onClick={handleLogin}
-                    style={{ 
-                        padding: "10px 20px", 
-                        backgroundColor: "#0070f3", 
-                        color: "white", 
-                        border: "none", 
-                        borderRadius: "5px",
-                        cursor: "pointer",
-                        width: "100%"
-                    }}
-                >
-                    Login (Navigasi ke Product)
-                </button>
-            </div>
-            
-            {/* Navigasi Link: Login ↔ Register */}
-            <p style={{ textAlign: "center" }}>
-                Belum punya akun? {" "}
-                <Link href="/auth/register" style={{ color: "#0070f3", textDecoration: "underline" }}>
-                    Register di sini
-                </Link>
-            </p>
+            <button onClick={() => handlerLogin()}>Login</button> <br />
+            <Link href="/auth/register">Ke Halaman Register</Link>
         </div>
     );
 };

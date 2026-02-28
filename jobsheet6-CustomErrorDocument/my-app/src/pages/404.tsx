289497f5132ddae982ddfb6bf34ddb64d0b69301
NathanaelGracedo <nathanjuan56@gmail.com>
2026-03-01 01:27:47 +0700

    Jobsheet 6 - Tugas Praktikum 1 - Menambahkan Judul halaman, Deskripsi Singkat, dan Gambar pada halaman 404

--- jobsheet6-CustomErrorDocument/my-app/src/pages/404.tsx
+++ jobsheet6-CustomErrorDocument/my-app/src/pages/404.tsx
@@ -2,9 +2,12 @@ import styles from '@/styles/404.module.scss';
 const Custom404 = () => {
     return(
         <div className={styles.error}>
+            <head>
+                <title>404 - Halaman Tidak Ditemukan</title>
+            </head>
             <img src="/404.png" alt="404" className={styles.error__image}/>
             <h1>404 - Halaman Tidak Ditemukan</h1>
-            <p>Maaf, halaman yang Anda cari tidak ditemukan.</p>
+            <p>Oops! Halaman anda telah dimakan</p>
         </div>
     );
 };

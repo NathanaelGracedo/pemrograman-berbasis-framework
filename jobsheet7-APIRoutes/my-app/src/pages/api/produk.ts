f1f169ede2b3b0b4a65c70dadfdb7c903a3c3304
NathanaelGracedo <nathanjuan56@gmail.com>
2026-03-01 20:13:26 +0700

    Jobsheet 07 - Langkah 10 - API Mengambil Data Firebase

--- jobsheet7-APIRoutes/my-app/src/pages/api/produk.ts
+++ jobsheet7-APIRoutes/my-app/src/pages/api/produk.ts
@@ -1,45 +1,21 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { retrieveProducts } from '../utils/db/servicefirebase';
 
 type Data = {
     status: boolean;
     status_code: number;
-    data: {
-        id: string;
-        nama: string;
-        harga: number;
-        ukuran: string;
-        warna: string;
-    }[];
+    data: any;
 };
 
-export default function handler(
+export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<Data>,
 )
     {
-        const data = 
-        [
-            {
-                id: '1',
-                nama: 'Baju',
-                harga: 100000,
-                ukuran: 'L',
-                warna: 'Merah'
-            },
-            {
-                id: '2',
-                nama: 'Celana',
-                harga: 150000,
-                ukuran: 'M',
-                warna: 'Hitam'
-            },
-            {
-                id: '3',
-                nama: 'Sepatu',
-                harga: 200000,
-                ukuran: '42',
-                warna: 'Putih'
-            },
-        ];
-        res.status(200).json({status: true, status_code: 200, data: data});
+        const data = await retrieveProducts("products");
+        res.status(200).json({
+            status: true,
+            status_code: 200,
+            data
+        });
     }

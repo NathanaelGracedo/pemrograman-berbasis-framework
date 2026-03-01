b088a2f1db453e25b86ad28363b71116c09b599e
NathanaelGracedo <nathanjuan56@gmail.com>
2026-03-01 19:24:25 +0700

    Jobsheet 7 - Langkah 3 - Fecth Data API di Frontend

--- jobsheet7-APIRoutes/my-app/src/pages/produk/index.tsx
+++ jobsheet7-APIRoutes/my-app/src/pages/produk/index.tsx
@@ -1,9 +1,41 @@
-import TampilanProduk from "../views/produk";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
-const Produk = () => {
+type ProductType = 
+{
+    id: string;
+    nama: string;
+    harga: number;
+    ukuran: string;
+    warna: string;
+}
+
+const kategori = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch('/api/produk')
+        .then((response) => response.json())
+        .then((responsedata) => {
+            setProducts(responsedata.data);
+        })
+        .catch((error) => {
+            console.error("Error fetching data: ", error);
+        });
+    }, []);
+
     return (
-        <TampilanProduk />
+        <div>
+            <h1>Daftar Produk</h1>
+            {products.map((product: ProductType) => (
+                <div key={product.id}>
+                    <h2>{product.nama}</h2>
+                    <p>Harga: {product.harga}</p>
+                    <p>Ukuran: {product.ukuran}</p>
+                    <p>Warna: {product.warna}</p>
+                </div>
+             ))}
+        </div>
     );
 };
 
-export default Produk;
+export default kategori;

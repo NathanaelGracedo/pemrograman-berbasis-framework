6ed2ac3f6de73b63017906cc71773df4ae5a7e0c
NathanaelGracedo <nathanjuan56@gmail.com>
2026-03-08 02:04:40 +0700

    Jobsheet 11 - Langkah 4 - Implementasi Static Site Generation (Dynamic SSG)

--- jobsheet11-DynamicRouting/my-app/src/pages/produk/[id].tsx
+++ jobsheet11-DynamicRouting/my-app/src/pages/produk/[id].tsx
@@ -25,9 +25,33 @@ const HalamanProduk = ({product}:{product: ProductType}) => {
 export default HalamanProduk;
 
 // digunakan server-side rendering
-export async function getServerSideProps({ params } : { params: { id: string}} ) {
-  const res = await fetch(`http://localhost:3000/api/produk/${params?.id}`);
+// export async function getServerSideProps({ params } : { params: { id: string}} ) {
+//   const res = await fetch(`http://localhost:3000/api/produk/${params?.id}`);
+//   const response = await res.json();
+//   return {
+//     props: {
+//       product: response.data,
+//     },
+//   };
+// }
+
+// digunakan static-site generation
+export async function getStaticPaths() {
+  const res = await fetch(`http://localhost:3000/api/produk`);
   const response = await res.json();
+
+  const paths = response.data.map((product: ProductType) => ({
+    params: { id: product.id }
+  }))
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps({ params } : { params: { id: string}} ) {
+  const res = await fetch(`http://localhost:3000/api/produk/${params?.id}`);
+  const response : { data: ProductType[] } = await res.json();
   return {
     props: {
       product: response.data,
--- jobsheet11-DynamicRouting/my-app/src/views/DetailProduk/index.tsx
+++ jobsheet11-DynamicRouting/my-app/src/views/DetailProduk/index.tsx
@@ -7,7 +7,7 @@ const DetailProduk = ({ products } : { products: ProductType }) => {
         <h1 className={styles.title}>Detail Produk</h1>
         <div className={styles.produkdetail}>
             <div className={styles.produkdetail_image}>
-                <img src={products.image} alt={products.name} />
+                <img src={products.image && products.image } alt={products.name} />
             </div>
 
             <div className={styles.produkdetail_info}>

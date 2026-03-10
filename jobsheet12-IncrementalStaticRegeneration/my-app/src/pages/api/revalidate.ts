1cc5d8bc91efc173d08b3ef08a452c6e56d27ce3
NathanaelGracedo <nathanjuan56@gmail.com>
2026-03-10 20:45:15 +0700

    Jobsheet 12 - Langkah 4 - Menambahkan Parameter Data

---
+++ jobsheet12-IncrementalStaticRegeneration/my-app/src/pages/api/revalidate.ts
@@ -0,0 +1,26 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+type Data = {
+    revalidated: boolean;
+    message?: string;
+};
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
+) {
+    if (req.query.data === "produk") {
+        try {
+            await res.revalidate('/produk/static');
+            return res.status(200).json({ revalidated: true });
+        } catch (error) {
+            console.error("Error in API route:", error);
+            return res.status(500).json({ revalidated: false });
+        }
+    }
+
+    return res.json({
+        revalidated: false,
+        message: "Invalid query parameter. Expected 'data=produk'."
+    });
+}

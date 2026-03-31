09a95216090898de1b2c2aab773d48a4f7dca2dc
NathanaelGracedo <nathanjuan56@gmail.com>
2026-03-31 19:48:46 +0700

    Jobsheet 13 - Langkah 1 - Membuat Middleware

--- jobsheet13-MiddlewareRouteProtection/my-app/src/pages/produk/index.tsx
+++ jobsheet13-MiddlewareRouteProtection/my-app/src/pages/produk/index.tsx
@@ -1,22 +1,18 @@
 import useSWR from "swr";
 import fetcher from "../../utils/swr/fetcher";
 import TampilanProduk from "../../views/produk";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 const kategori = () => {
-    const { data, error, isLoading, mutate } = useSWR("/api/produk", fetcher);
+    const {push} = useRouter();
+    const [products, setProducts] = useState([]);
 
-    const handleRefresh = () => {
-        mutate();
-    };
-
-    const products = data?.data || [];
+    const {data, error, isLoading } = useSWR("/api/products", fetcher);
 
     return (
         <div>
-            <button className="border-2 border-black p-2 rounded mb-4 ml-8 mt-4" onClick={handleRefresh}>
-                Refresh Data
-            </button>
-            <TampilanProduk products={products} isLoading={isLoading} />
+            <TampilanProduk products={isLoading ? [] : data.data } />
         </div>
     );
 };

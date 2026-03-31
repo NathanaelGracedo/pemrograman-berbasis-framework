98175001acec066d369eb312fa78883248f62473
NathanaelGracedo <nathanjuan56@gmail.com>
2026-03-31 21:41:22 +0700

    Jobsheet 14 - Langkah 4 - Menambahkan SessionProvider

--- jobsheet14-AutentikasidanProteksiRoute/my-app/src/pages/_app.tsx
+++ jobsheet14-AutentikasidanProteksiRoute/my-app/src/pages/_app.tsx
@@ -1,14 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
 // import Navbar from '@/components/layouts/navbar'
-import AppShell from '@/components/layouts/Appshell'
+import AppShell from '@/components/layouts/Appshell';
+import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <div>
+    <SessionProvider session={pageProps.session}>
       <AppShell>
         <Component {...pageProps} />
       </AppShell>
-    </div>
+    </SessionProvider>
   );
 };

300622e5e9bb47c116ef7faf909c17e55478d038
NathanaelGracedo <nathanjuan56@gmail.com>
2026-04-04 20:08:38 +0700

    Jobsheet 15 - Tugas Praktikum 3 - Mengubah role default user menjadi "member"

--- jobsheet15-AuthRegister/my-app/src/utils/db/servicefirebase.ts
+++ jobsheet15-AuthRegister/my-app/src/utils/db/servicefirebase.ts
@@ -58,7 +58,7 @@ export async function signUp(
   } else {
     // User belum ada, tambahkan ke database
     userData.password = await bcrypt.hash(userData.password, 10);   
-    userData.role = "user"; 
+    userData.role = "member"; 
     await addDoc(collection(db, "users"), userData)
         .then(() => {
             callback({

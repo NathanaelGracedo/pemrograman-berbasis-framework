9f0277e5655e3f769219ae72e06e352a2c4db262
NathanaelGracedo <nathanjuan56@gmail.com>
2026-04-07 01:53:06 +0700

    Jobsheet 18 - Tugas Praktikum - Menambahkan script google analytics

--- jobsheet18-Optimize/my-app/src/pages/_app.tsx
+++ jobsheet18-Optimize/my-app/src/pages/_app.tsx
@@ -1,15 +1,40 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 // import Navbar from '@/components/layouts/navbar'
 import AppShell from '@/components/layouts/Appshell';
 import { SessionProvider } from "next-auth/react";
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  const GA_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
+
   return (
-    <SessionProvider session={session}>
-      <AppShell>
-        <Component {...pageProps} />
-      </AppShell>
-    </SessionProvider>
+    <>
+      <SessionProvider session={session}>
+        <AppShell>
+          <Component {...pageProps} />
+        </AppShell>
+      </SessionProvider>
+      
+      {/* Google Analytics */}
+      {GA_ID && (
+        <>
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_ID}', {
+                page_path: window.location.pathname,
+              });
+            `}
+          </Script>
+        </>
+      )}
+    </>
   );
 };

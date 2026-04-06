109dc4d9102f2d7cbdac5f2669772f45f270db27
NathanaelGracedo <nathanjuan56@gmail.com>
2026-04-07 00:53:27 +0700

    Jobsheet 18 - Langkah 2 - Font Optimization menggunakan next/font

--- jobsheet18-Optimize/my-app/src/components/layouts/Appshell/index.tsx
+++ jobsheet18-Optimize/my-app/src/components/layouts/Appshell/index.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import Navbar from "../navbar";
 import Footer from "../footer";
+import { Roboto } from "next/font/google";
 
 const disableNavbar = ["/auth/login", "/auth/register", "/views/auth/login", "/views/auth/register", "/404"];
 
@@ -8,11 +9,16 @@ type AppShellProps = {
     children: React.ReactNode;
 }
 
+const roboto = Roboto({
+    subsets: ['latin'],
+    weight: ['400', '500', '700'],
+})
+
 const AppShell = (props: AppShellProps) => {
     const { children} = props;
     const { pathname } = useRouter();
     return (
-        <main>
+        <main className={roboto.className}>
             {!disableNavbar.includes(pathname) && <Navbar></Navbar>}
             {children}
             <Footer></Footer>

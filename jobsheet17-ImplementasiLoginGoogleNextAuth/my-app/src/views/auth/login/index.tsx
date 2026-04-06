0f5c02ab26a1728cbd4670642b35286fc7b927ff
NathanaelGracedo <nathanjuan56@gmail.com>
2026-04-06 21:54:55 +0700

    Jobsheet 17 - Langkah E - Menambahkan Button Login Google

--- jobsheet17-ImplementasiLoginGoogleNextAuth/my-app/src/views/auth/login/index.tsx
+++ jobsheet17-ImplementasiLoginGoogleNextAuth/my-app/src/views/auth/login/index.tsx
@@ -79,6 +79,11 @@ const TampilanLogin = () => {
                     <button type="submit" disabled={isLoading} className={style.login__form__item__button}>
                         {isLoading ? "Loading..." : "login"}
                     </button>
+                    <br />
+                    <br />
+                    <button onClick={() => signIn("google", { callbackUrl, redirect: false })} className={style.login__form__item__button} disabled={isLoading}>
+                        {isLoading ? "Loading..." : "Sign in with Google"}
+                    </button>
                 </form>
                 <br />
                 <p className={style.login__form__item__text}>

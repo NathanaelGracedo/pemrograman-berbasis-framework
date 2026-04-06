1416fa36c457439fa02d00a935ed8ea5ce1c3930
NathanaelGracedo <nathanjuan56@gmail.com>
2026-04-06 23:16:41 +0700

    Jobsheet 17 - Tugas Praktikum 5 - Mengubah avatar agar menggunakan next/image guna optimasi

--- jobsheet17-ImplementasiLoginGoogleNextAuth/my-app/next.config.js
+++ jobsheet17-ImplementasiLoginGoogleNextAuth/my-app/next.config.js
@@ -4,6 +4,22 @@ const nextConfig = {
   compiler: {
     removeConsole: false,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.googleusercontent.com',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig

2d995838b2e604376b910ffd1ad18ce3e49e7dc2
NathanaelGracedo <nathanjuan56@gmail.com>
2026-04-07 00:29:37 +0700

    Jobsheet 18 - Optimize

---
+++ jobsheet18-Optimize/my-app/next.config.js
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  compiler: {
+    removeConsole: false,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.googleusercontent.com',
+      },
+    ],
+  },
+}
+
+module.exports = nextConfig
---
+++ jobsheet18-Optimize/my-app/src/pages/editor/index.tsx
@@ -0,0 +1,12 @@
+const halamanEditor = () => {
+    return (
+        <div>
+            <div className="editor">
+                <h1>Halaman Editor</h1>
+                <p>Selamat Datang Di halaman Editor!</p>
+            </div>
+        </div>
+    );
+};
+
+export default halamanEditor;

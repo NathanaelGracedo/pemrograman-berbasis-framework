5f998a53fd02d5533d7b967cdccfc91af7e02b9f
NathanaelGracedo <nathanjuan56@gmail.com>
2026-04-06 20:54:52 +0700

    Jobsheet 17 - Implementasi Login Google Provider dengan NextAuth,js

---
+++ jobsheet17-ImplementasiLoginGoogleNextAuth/my-app/src/pages/admin/index.tsx
@@ -0,0 +1,12 @@
+const halamanAdmin = () => {
+    return (
+        <div>
+            <div className="admin">
+                <h1>Halaman Admin</h1>
+                <p>Selamat Datang Di halaman Admin!</p>
+            </div>
+        </div>
+    );
+};
+
+export default halamanAdmin;
